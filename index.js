27cb670baa656c6ea27ac1cced8db9dad8f80d49
CourtneyKerr19 <court.knee219@gmail.com>
2025-04-05 05:09:07 -0500

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+//employee object
+const employee = {
+    name: 'Spongebob',
+    streetAddress: 'Pineapple under the sea'
+};
+
+// updateEmployeeWithKeyAndValue function
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {...employee, [key]: value};
+}
+
+// destructivelyUpdateEmployeeWithKeyAndValue function
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+// deleteFromEmployeeByKey function
+function deleteFromEmployeeByKey(employee, key){
+     const { [key]: deletedKey, ...newEmployee } = employee;
+     return newEmployee;
+}
+
+// destructivelyDeleteFromEmployeeByKey function
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
